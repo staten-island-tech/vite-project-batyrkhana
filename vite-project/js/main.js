24c7aa458e81d83e7ef9d9fd3d466094c41aaeb5
batyrkhana <batyrkhansa@gmail.com>
2023-11-14 09:21:43 -0500

    11/14/2023

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,3 +1,32 @@
 import "../styles/style.css";
 import { vinyls } from "./vinyls";
 console.log(vinyls);
+
+function createAlbum() {
+    DOMSelectors.box.insertAdjacentHTML("beforeend",
+  `<div class="create-album">
+      <img src=${DOMSelectors.albumImage.value} alt="Album Cover" class="card-img">
+      <h1 class="card-albumName">${DOMSelectors.albumName.value}</h1>
+      <h2 class="card-release">${DOMSelectors.albumReleaseDate.value}</h2>
+      <h3 class="card-artist">${DOMSelectors.albumArtist.value}</h3>
+      <button class="delete">DELETE</button>
+    </div>`);
+}
+
+createAlbum();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
