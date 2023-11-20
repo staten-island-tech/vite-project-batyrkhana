fbd850b89f276b7a2c387427dfe5cd53e33540fa
batyrkhana <batyrkhansa@gmail.com>
2023-11-19 22:28:09 -0500

    Design and Theme Selection
    Completely overhauled the design of the website, creating a new theme. Made the website more appealing to the eye.

--- vite-project/js/Dom.js
+++ vite-project/js/Dom.js
@@ -1,4 +1,10 @@
 const DOMSelectors = {
     products: document.getElementById("products"),
+    changeTheme: document.getElementById("changeTheme"),
+    allVinyls: document.getElementById("allVinyls"),
+    classicRock: document.getElementById("classicRock"),
+    country: document.getElementById("country"),
+    priceHighLo: document.getElementById("priceHighLo"),
+    priceLowHi: document.getElementById("priceLowHi"),
 }
 export {DOMSelectors};
